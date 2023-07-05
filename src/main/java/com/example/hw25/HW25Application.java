aec63277dfe71d0ad013c2fe5a2f5936386b64b6
RenaProsto <renaprosto123@gmail.com>
2023-07-05 12:48:03 +0500

    первый коммит

---
+++ src/main/java/com/example/hw25/HW25Application.java
@@ -0,0 +1,13 @@
+package com.example.hw25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HW25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HW25Application.class, args);
+	}
+
+}
